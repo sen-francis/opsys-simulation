a0b3bd98e43d13dae6c7cd8f25468b0af0274af1
Sandor Magyar <sandorzm@gmail.com>
2022-03-20 23:37:16 -0400

    Get newest version of output files (fix tau calcs)

--- runall.sh
+++ runall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f a.out
-gcc -Wall main.c -lm
+gcc -Wall src/*.c -lm
 
 rm -f output/output*.txt
 rm -f output/simout*.txt
@@ -20,7 +20,7 @@ a.out 8 101 0.001 16384 4 0.5 2048 > output/output05-full.txt
 
 
 # test _1 compile (gcc; g++; javac; python -m py_compile p1.py)
-gcc -Wall main.c -lm -D DISPLAY_MAX_T=1000
+gcc -Wall src/*.c -lm -D DISPLAY_MAX_T=1000
 
 # test _2
 a.out 1 9 0.01 4096 4 0.5 256 > output/output02.txt
